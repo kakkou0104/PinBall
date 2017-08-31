e41d3c3802bdd5a4b3e3c49a731a40002f00903a
kakkou0104 <kakkou0104@gmail.com>
2017-09-01 08:51:49 +0900

    PinBall 第一回コミット
    ①カリキュラムに従ったプログラムの作成、②Scoreに基本課題、③TouchControllerに発展課題

---
+++ Assets/Score.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Score : MonoBehaviour {
+
+	private int score = 0;
+
+	//スコアを表示するテキスト
+	public GameObject scoreText;
+
+	// Use this for initialization
+	void Start () {
+		//シーン中のScoreTextオブジェクトを取得
+		this.scoreText = GameObject.Find ("ScoreText");
+		score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	//衝突時に呼び出される関数
+	void OnCollisionEnter(Collision collision){
+
+		//衝突したオブジェクトのタグによって得点を加算
+		if (collision.gameObject.tag == "SmallStarTag") {
+			score += 10;
+		} else if (collision.gameObject.tag == "LargeStarTag") {
+			score += 20;
+		} else if (collision.gameObject.tag == "SmallCloudTag") {
+			score += 30;
+		} else if (collision.gameObject.tag == "LargeCloudTag") {
+			score += 40;
+		}
+
+		//SetScore関数の呼び出し
+		SetScore ();
+	}
+
+	//SetScore関数の宣言
+	void SetScore(){
+		//ScoreTextにスコアを表示
+		this.scoreText.GetComponent<Text> ().text = "score : " + score;
+	}
+
+}
---
+++ Assets/TouchController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour {
+
+	//HingeJointコンポーネントを入れる
+	private HingeJoint myHingeJoint;
+	//初期の傾き
+	private float defaultAngle = 30;
+	//弾いた時の傾き
+	private float flickAngle = -20;
+
+	// Use this for initialization
+	void Start () {
+		//HinjiJointコンポーネント取得
+		this.myHingeJoint = GetComponent<HingeJoint> ();
+		//フリッパーの傾きを設定
+		SetAngle (this.defaultAngle);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (0 < Input.touchCount) {
+			// タッチされている指の数だけ処理
+			for (int i = 0; i < Input.touchCount; i++) {
+				// タッチ情報をコピー
+				Touch tch = Input.GetTouch (i);
+			
+				//画面左をタッチした時左フリッパーを動かす
+				if (tch.phase == TouchPhase.Began && Screen.width / 2 > tch.position.x && tag == "LeftFripperTag") {
+					SetAngle (this.flickAngle);
+
+				}
+		//画面右をタッチした時右フリッパーを動かす
+		else if (tch.phase == TouchPhase.Began && Screen.width / 2 < tch.position.x && tag == "RightFripperTag") {
+					SetAngle (this.flickAngle);
+
+				}
+		//画面から手を離すとフリッパーを元に戻す
+				else if (tch.phase == TouchPhase.Ended && Screen.width / 2 > tch.position.x && tag == "LeftFripperTag") {
+					SetAngle (this.defaultAngle);
+				} 
+				else if (tch.phase == TouchPhase.Ended && Screen.width / 2 < tch.position.x && tag == "RightFripperTag") {
+					SetAngle (this.defaultAngle);
+				}
+			}
+		}
+	}
+	
+			
+	public void SetAngle (float angle){
+		JointSpring jointSpr = this.myHingeJoint.spring;
+		jointSpr.targetPosition = angle;
+		this.myHingeJoint.spring = jointSpr;
+	}
+}
